7170e8a64de15a24b0c9a6c37e88d3d1e555fabc
Martin Ovsenak <xmarty@email.cz>
2019-08-07 15:44:48 +0200

    add togglebox resizing, auto unchecking

---
+++ serialservo2/serialservo2.ino
@@ -0,0 +1,104 @@
+/*
+  
+*/
+
+#include <Servo.h>
+#include <Arduino.h>
+
+
+Servo servoX;  // create servo object to control a servo
+Servo servoY;  // create servo object to control a servo
+// int val;    // variable to read the value from the analog pin
+int maxcycles = 5; //set by pc?. as variable ? -  this value limits max cycles before return to neutrál
+int counter;  //used for maxcycles counting
+int maxduration = 8000; // this value we get from pc
+int neutralpos = 90;  //servo middle position
+int neutraltime = 300;  // how long we stay at middle position after move
+int xpos, ypos, duration;
+int cycledelay, delaycounter;  //  for the move duration cycle
+
+
+void setup() {
+  // set initial values:
+  xpos = ypos = neutralpos; // set to middle position
+  cycledelay = 100;  // intervals, in which we check serial for new data, until 'duration' time is reached
+  // initialize servos:s
+  servoX.attach(9); 
+  servoX.write(neutralpos);                  // sets the servo position according to the scaled value
+  servoY.attach(10); 
+  servoY.write(neutralpos); 
+  
+  delay(1500); // let the servos move to position
+  // initialize serial comm:
+  Serial.begin(9600); 
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+}
+
+
+void loop() {
+  //security counter- if we receive no data for maxtime, we reset servo position to 90 deg - needs revision, may be useless:
+  counter = maxcycles;
+      // Read serial input - three integer values between ( ) otherwise skip to next: 
+  if (Serial.available() > 0) {
+    char inChar = Serial.read(); // read one char
+    Serial.print("inChar: ");
+    Serial.println(inChar);
+    // check if we are at start of the message:
+    if (inChar == '(') {
+      // parsing message (xpos:ypos:duration) 
+      xpos = Serial.parseInt(); // read 1st integer
+      ypos = Serial.parseInt(); // read 2nd integer
+      duration = Serial.parseInt();  // read 3rd integer
+      inChar = Serial.read();  // read end of message
+      // end of the message:
+      if (inChar == ')') {
+        // print for debug:
+        Serial.print("xpos: ");
+        Serial.println(xpos); 
+        Serial.print("ypos: ");
+        Serial.println(ypos);
+        Serial.print("duration: ");
+        Serial.println(duration);
+
+        // we need to check  values, then move servos:
+        if ((xpos <= 8) and (xpos >= 0) and (ypos <= 8) and (ypos >= 0) 
+               and (duration <= maxduration) and (duration >= 0)) {
+          xpos = map(xpos, 0, 8, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+          servoX.write(xpos);   
+          ypos = map(ypos, 0, 8, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+          servoY.write(ypos); // sets the servo position according to the scaled value
+         
+          
+          // clear any other chars from serial:
+          // debug
+          inChar = Serial.peek();
+          Serial.print("peek: ");
+          Serial.println(inChar);
+         
+          while ((inChar != -1) and (inChar != '(')) {
+            inChar = Serial.read();
+            Serial.print("read: ");
+            Serial.println(inChar);
+          }
+
+          // Now we wait - move duration. It's broken into short 'cycledelay' delays, 
+          // cause we want to be able to stop or change direction quickly any time,
+          // we can use switch or serial etc.:
+          delaycounter = 0;
+          while ((inChar = Serial.peek() == -1) and ((cycledelay*delaycounter) <= duration)) {
+            delay(cycledelay);
+            ++delaycounter;
+            Serial.print("peek2: ");
+            Serial.println(inChar);
+          }
+          servoX.write(neutralpos); 
+          servoY.write(neutralpos);// return to neutral
+          // delay(neutraltime); //without this, the servo has not time to return,
+          // if there's another serial input - this maybe desirable for smooth ride
+        }     
+      }
+    }
+  }
+} 
--- unit1.pas
+++ unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls,
-  Serial;
+  Serial, dbugintf;
 
 type
 
@@ -18,14 +18,33 @@ type
     ForwardTB: TToggleBox;
     BackwardsTB: TToggleBox;
     DurationSlider: TTrackBar;
+    FwdLeftTB: TToggleBox;
+    FwdRightTB: TToggleBox;
+    LeftTB: TToggleBox;
+    NopTB: TToggleBox;
+    RightTB: TToggleBox;
+    StopTB: TToggleBox;
+    Stop1TB: TToggleBox;
     UpDown1: TUpDown;
+
+    //procedure ForwardTBMouseEnter(Sender: TObject);
+    procedure ReleaseToggleboxes;
     procedure DurationSliderChange(Sender: TObject);
+    procedure FormResize(Sender: TObject);
+   //  procedure FormResize(Sender: TObject);
+    procedure FwdLeftTBChange(Sender: TObject);
+    procedure FwdRightTBChange(Sender: TObject);
+    procedure LeftTBChange(Sender: TObject);
     procedure PortEditChange(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure ForwardTBChange(Sender: TObject);
     procedure MoveJoystick(xpos:integer;ypos:integer);
     procedure BackwardsTBChange(Sender: TObject);
+    procedure RightTBChange(Sender: TObject);
+    procedure StopTBChange(Sender: TObject);
+    procedure Stop1TBChange(Sender: TObject);
     procedure UpDown1Click(Sender: TObject; Button: TUDBtnType);
+    procedure Stop();
 
   private
 
@@ -43,7 +62,7 @@ var
   speed,duration:integer;  // global setting , that affects joystick movement
   status: LongInt;
   Xposition,Yposition: Integer;  // numbers forwarded as parameters to Move Joystick procedure
-  hoverdrive, charged: boolean; // mouse click control or both click hover/eyegaze/
+  hoverdrive: boolean; // mouse click control or both click hover/eyegaze/
   // control. if charged we can move - charging done by activating charge button-this prevents stuck cursor driving
 implementation
 
@@ -53,8 +72,8 @@ implementation
 
 procedure TForm1.ForwardTBChange(Sender: TObject);
 begin
-// add security delay, in which we can abort action ? should be in hover setion only, and variable
-  // forward
+
+  ReleaseToggleboxes;
   Xposition := midpos + speed;
   Yposition := midpos;
   MoveJoystick(Xposition,Yposition);
@@ -62,12 +81,12 @@ end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
+  Label1.Caption :=  IntToStr(UpDown1.Position);        // Display initial speed value
   speed := UpDown1.Position; // set from 0 to 4, affects both y and x positions of the joystick
   duration := DurationSlider.Position;    // after this time joystick returns to middle position
   portname := PortEdit.Text;
   hoverdrive := False; // if enabled, we activate move by either
              // mouse hover and click , if disabled, only click works
-  charged := True;
 end;
 
 procedure TForm1.PortEditChange(Sender: TObject);
@@ -80,6 +99,69 @@ begin
    duration := DurationSlider.Position;
 end;
 
+
+
+procedure TForm1.FormResize(Sender: TObject);
+var
+  Temp: TToggleBox;
+  tilewidth, tileheight, i: integer;
+begin
+  // adjust all toggleboxes sizes
+  tileheight:= (Form1.Height) div 3;
+  tilewidth := (Form1.Width - (UpDown1.Width + 20 + DurationSlider.Width)) div 3;
+  //SendDebug(inttostr(tilewidth));
+  //SendDebug(inttostr(tileheight));
+
+  ForwardTB.Height:=tileheight;
+  FwdLeftTB.Height:=tileheight;
+  LeftTB.Height:=tileheight;
+  Stop1TB.Height:=tileheight;
+  BackwardsTB.Height:=tileheight;
+  StopTB.Height:=tileheight;
+  RightTB.Height:=tileheight;
+  FwdRightTB.Height:=tileheight;
+  NopTB.Height:=tileheight;
+
+  ForwardTB.Width:=tilewidth;
+  FwdLeftTB.Width:=tilewidth;
+  LeftTB.Width:=tilewidth;
+  Stop1TB.Width:=tilewidth;
+  BackwardsTB.Width:=tilewidth;
+  StopTB.Width:=tilewidth;
+  RightTB.Width:=tilewidth;
+  FwdRightTB.Width:=tilewidth;
+  NopTB.Width:=tilewidth;
+end;
+
+procedure TForm1.FwdLeftTBChange(Sender: TObject);
+begin
+  ReleaseToggleboxes;
+  //  forward left
+  Xposition := midpos + speed;
+  Yposition := midpos - speed;
+  MoveJoystick(Xposition,Yposition);
+end;
+
+procedure TForm1.FwdRightTBChange(Sender: TObject);
+begin
+  ReleaseToggleboxes;
+  //  forward right
+  Xposition := midpos + speed;
+  Yposition := midpos + speed;
+  MoveJoystick(Xposition,Yposition);
+end;
+
+procedure TForm1.LeftTBChange(Sender: TObject);
+begin
+  ReleaseToggleboxes;
+  // turn Left
+  Xposition := midpos;
+  Yposition := midpos + speed;
+  MoveJoystick(Xposition,Yposition);
+end;
+
+
+
 procedure TForm1.MoveJoystick(xpos:integer;ypos:integer);
 // this procedure opens serial port, sends message to arduino, closes the port
 // the message tells to which x and y positions will servos move and for how long (DurationSlider)
@@ -89,34 +171,37 @@ var
   message: String;
   status: LongInt;
 begin
-  serHandle := SerOpen(portname); // Bei Windows 'COMx' // COM-Port Ã¶ffnen.
-  // writeln(serHandle,' ',message,' ', portname);
-  message := '(' + IntToStr(xpos) + ':' + IntToStr(ypos) + ':' + IntToStr(duration) + ')';
-  SerSetParams(serHandle, 9600, 8, NoneParity, 1, []);
-  msglen := length(message);
-  for i := 1 to msglen do begin
-      status := SerWrite(serHandle, message[i], 1); // Zeichen senden.
-  end;
 
-  message := '';
-  if(status > 0) then begin
-    // writeln('written to serial port');
-  end else begin
-    // writeln('can not write to serial port, quitting');
+    serHandle := SerOpen(portname); // Bei Windows 'COMx' // COM-Port Ã¶ffnen.
+    // writeln(serHandle,' ',message,' ', portname);
+    message := '(' + IntToStr(xpos) + ':' + IntToStr(ypos) + ':' + IntToStr(duration) + ')';
+    SerSetParams(serHandle, 9600, 8, NoneParity, 1, []);
+    msglen := length(message);
+    for i := 1 to msglen do begin
+        status := SerWrite(serHandle, message[i], 1); // Zeichen senden.
+    end;
+
+    message := '';
+    if(status > 0) then begin
+      // writeln('written to serial port');
+    end else begin
+      // writeln('can not write to serial port, quitting');
+      SerSync(serHandle); { flush out any remaining before closure }
+      SerFlushOutput(serHandle); { discard any remaining output }
+      SerClose(serHandle);
+      // report error
+    end;
+    //arduino response code here:
+    //
     SerSync(serHandle); { flush out any remaining before closure }
     SerFlushOutput(serHandle); { discard any remaining output }
-    SerClose(serHandle);
-    // report error
-  end;
-  //arduino response code here:
-  //
-  SerSync(serHandle); { flush out any remaining before closure }
-  SerFlushOutput(serHandle); { discard any remaining output }
-  SerClose(serHandle);            // COM-Port schliessen.
+    SerClose(serHandle);            // COM-Port schliessen.
+
 end;
 
 procedure TForm1.BackwardsTBChange(Sender: TObject);
 begin
+    ReleaseToggleboxes;
   // back
 
   Xposition := midpos - speed;
@@ -125,11 +210,49 @@ begin
 
 end;
 
+procedure TForm1.RightTBChange(Sender: TObject);
+begin
+  ReleaseToggleboxes;
+  // turn right
+  Xposition := midpos;
+  Yposition := midpos + speed;
+  MoveJoystick(Xposition,Yposition);
+end;
+
+procedure TForm1.StopTBChange(Sender: TObject);
+begin
+  ReleaseToggleboxes;
+  Stop();
+end;
+
+procedure TForm1.Stop1TBChange(Sender: TObject);
+begin
+  ReleaseToggleboxes;
+   Stop();
+end;
+procedure TForm1.Stop();
+begin
+  Xposition := midpos;
+  Yposition := midpos;
+  MoveJoystick(Xposition,Yposition);
+end;
+
 procedure TForm1.UpDown1Click(Sender: TObject; Button: TUDBtnType);
 begin
   Label1.Caption :=  IntToStr(UpDown1.Position);
   speed := UpDown1.Position;
 end;
-
+procedure TForm1.ReleaseToggleboxes;
+begin
+  ForwardTB.State:=cbUnchecked;
+  FwdLeftTB.State:=cbUnchecked;
+  LeftTB.State:=cbUnchecked;
+  Stop1TB.State:=cbUnchecked;
+  BackwardsTB.State:=cbUnchecked;
+  StopTB.State:=cbUnchecked;
+  RightTB.State:=cbUnchecked;
+  FwdRightTB.State:=cbUnchecked;
+  NopTB.State:=cbUnchecked;
+end;
 end.
 
